7d4a97c0efde3eef9859b06dd75833130f982e36
Handika Rado Arganata <arganata.on@gmail.com>
2024-10-15 23:12:35 +0700

    Tugas_Individu_03_Perulangan.cpp

--- Tugas_Individu_03_Perulangan.cpp
+++ Tugas_Individu_03_Perulangan.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    int angka = 0, input, jumlahGanjil = 0,jumlahGenap = 0;
+int main() {
+    int angka = 0, input, jumlahGanjil = 0, jumlahGenap = 0;
     cout << "Masukan jumlah data : ";
     cin >> input;
-    for(angka = 0; angka < input; angka++){
-        if(angka % 2 == 0){
+    for (angka = 0; angka < input; angka++) {
+        if (angka % 2 == 0) {
             jumlahGenap += angka;
         }
-        else{
+        else {
             jumlahGanjil += angka;
         }
     }
